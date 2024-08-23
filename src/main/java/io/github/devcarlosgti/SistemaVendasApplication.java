6d761e733191c7d3fabc3d33705bdd73ac189019
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-08-23 10:55:04 -0300

    Criação do projeto Sistema de Vendas

---
+++ src/main/java/io/github/devcarlosgti/SistemaVendasApplication.java
@@ -0,0 +1,13 @@
+package io.github.devcarlosgti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendasApplication {
+
+    //atalho para main(psvm)
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaVendasApplication.class, args);
+    }
+}
